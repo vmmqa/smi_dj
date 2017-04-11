3fb09b7b8c86c45f274e57a86e6d6aaf9cba6040
Libo Zhu <libo.zhu@intel.com>
2017-04-11 13:09:57 +0800

    v1 to add mulit value support
    it will accept mulit values by -r or --run option
    -t or --testplan to get the specified testplan absolute file
    
    Signed-off-by: Libo Zhu <libo.zhu@intel.com>

--- smi-server.py
+++ smi-server.py
@@ -1,7 +1,9 @@
 # Written by Libo
 import os
+import sys
 import multiprocessing
 import time
+from optparse import OptionParser  
 #==================
 # input worker
 def inputQ(queue,i):
@@ -27,6 +29,15 @@ def outputQ(queue,lock):
 #===================
 # Main
 if __name__ == '__main__':
+    optParser = OptionParser()
+    optParser.add_option("-t","--testplan",action = "store",type="string",dest = "testplan")
+    optParser.add_option("-r","--run",action = "append",dest = "run")
+    #fakeArgs = ['-f','file.txt','-v','good luck to you', 'arg2', 'arge','-f','file2.txt']
+    #fakeArgs = ['-r','ip:192;broadinfo:xiaoming;user:xiaoming','-r','ip:193']  
+    #options, args = optParser.parse_args(fakeArgs)
+    options, args = optParser.parse_args()
+    print(type(options.run),options.run)
+    sys.exit()
     record1 = []   # store input processes
     record2 = []   # store output processes
     lock  = multiprocessing.Lock()    # To prevent messy print
