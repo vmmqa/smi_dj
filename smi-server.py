1e912ab8197a5e76e334c32dafe3aab1751098b1
Libo Zhu <libo.zhu@intel.com>
2017-04-14 03:27:11 +0800

    add multi process lanuch support
    the input's format support as follows:
    a: -r entry -r entry ...
    b: --run entry --run entry ...
    c: --run=entry --run=entry ...
    
    the syntax of entry is:
    	ip:xxxx;broadinfo:xxxx;username:xxxx
    
    Signed-off-by: Libo Zhu <libo.zhu@intel.com>

--- smi-server.py
+++ smi-server.py
@@ -4,6 +4,7 @@ import os
 import sys
 import multiprocessing
 import time
+import random
 from optparse import OptionParser  
 #==================
 # input worker
@@ -14,10 +15,19 @@ def inputQ(queue,i):
 def inputQ(queue,key, command):
     #info = str(os.getpid()) + '(put):' + str(time.time()) +'(number):'+str(i)
     #print(info)
-    localcmd='staf '+key+' '+command
+    #localcmd='staf '+key+' '+command
+    localcmd='staf ' +key+' process start shell command '
+    localcmd+=command
+    localcmd+=' wait returnstdout'
     process=os.popen(localcmd)
     print(localcmd)
-    output=process.read()
+    tr=random.uniform(1,10) 
+    print('tr=',tr)
+    time.sleep(tr)
+    output="the result of "
+    output+=key
+    output+='\n'
+    output+=process.read()
     process.close()
     queue.put(output)
     
@@ -32,12 +42,14 @@ def outputQ(queue,lock):
 if __name__ == '__main__':
     optParser = OptionParser()
     optParser.add_option("-t","--testplan",action = "store",type="string",dest = "testplan")
+    optParser.add_option("-w","--workspace",action = "store",type="string",dest = "workspace",default="/home/sky-nuc-libo1/repos/dji/example/")
     optParser.add_option("-r","--run",action = "append",dest = "run")
     #fakeArgs = ['-f','file.txt','-v','good luck to you', 'arg2', 'arge','-f','file2.txt']
     #fakeArgs = ['-r','ip:192;broadinfo:xiaoming;user:xiaoming','-r','ip:193']  
     #options, args = optParser.parse_args(fakeArgs)
     options, args = optParser.parse_args()
     print(type(options.run),options.run)
+    print(type(options.testplan),options.testplan)
     record1 = []   # store input processes
     record2 = []   # store output processes
     lock  = multiprocessing.Lock()    # To prevent messy print
@@ -48,21 +60,22 @@ if __name__ == '__main__':
     for item in options.run:
         ipl.append(smiCommon.cmdparse(item))
     print(ipl)
-    sys.exit()
+    #sys.exit()
     
-    dic = {'192.168.79.1':'service list', 'local':'var list','192.168.79.2':'help help'}
-
+    #dic = {'192.168.79.1':'service list', 'local':'var list','192.168.79.2':'help help'}
+    #dic = {'10.239.196.96':'calc', 'local':'ls -l','10.239.159.58':'sleep 10s'}
+    cmd="sleep 2s"
     # input processes
-    #for i in range(10):
-    for key in dic:
+    #for key in dic:
         #process = multiprocessing.Process(target=inputQ,args=(queue,i))
-        process = multiprocessing.Process(target=inputQ,args=(queue,key,dic[key]))
+    for key in ipl:
+        process = multiprocessing.Process(target=inputQ,args=(queue,key,cmd))
         process.start()
         record1.append(process)
 
     # output processes
-    #for i in range(10):
-    for key in dic:
+    #for key in dic:
+    for key in ipl:
         process = multiprocessing.Process(target=outputQ,args=(queue,lock))
         process.start()
         record2.append(process)
