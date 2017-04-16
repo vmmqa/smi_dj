1b954569171c0b7d54509990a06df94b17e55608
Libo Zhu <libo.zhu@intel.com>
2017-04-16 14:20:44 +0800

    update register and unregister logic
    the user only needs input the server ip and boradid
    
    Signed-off-by: Libo Zhu <libo.zhu@intel.com>

--- smi-cli.py
+++ smi-cli.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     usage = "usage: %prog [-r|-u boardid] [-f] [-i server's ip]"
     parser = OptionParser(usage=usage)
     parser.add_option("-r", "--register", type="string", dest="register",  
-                  help="format, ip:xxx;username:xxx;user:xxx")
+                  help="format, board")
     parser.add_option("-i","--ip",type="string",dest="ipaddr",
                   help="please input the sever's ip",default="local")
     parser.add_option("-u","--unregister",type="string",dest="unregister",
@@ -25,43 +25,59 @@ if __name__ == '__main__':
     ip=socket.gethostbyname(socket.gethostname())
     username=socket.gethostname()
     userpc=os.getlogin()
-    entry=None
+    entry=str()
     print("ip=",ip,",username=",username,",userpc=",userpc)
-    sys.exit()
-    if options.register!=None:
-		print('it is for register action')
-		cmd=['staf',options.ipaddr,'respool','add','pool','dji','entry',options.register]
-		#p=subprocess.Popen(['staf',options.ipaddr,'respool','add','pool','dji','entry',options.register])
-		print("cmd=",cmd)
-		p=subprocess.Popen(cmd)
-		p.wait()
-		print('returncode=',p.returncode)
-		if p.returncode==0:
-			print("pass to reigster\n")
-		elif p.returncode==49:
-			print("already exists\n")
-		else:
-			print("fail to register\n")
-    elif options.unregister!=None:
-		print('it is for unregister action')
-		postcmd='CONFIRM'
-		cmd=['staf',options.ipaddr,'respool','remove','pool','dji','entry',options.unregister, 'confirm']
-		if options.force==True:
-			cmd.append('FORCE')
-		print("cmd=",cmd)
-		p=subprocess.Popen(cmd)
-		p.wait()
-		print('returncode=',p.returncode)
-		if p.returncode==0:
-			print("pass to unreigster\n")
-		elif p.returncode==48:
-			print("it does NOT exist\n")
-		elif p.returncode==4010:
-			print(" A resource pool entry you specified to REMOVE is owned.Use the -f or --force option if you are sure that the correct\
-			      entry is specficed")
-		else:
-			print("fail to unregister\n")
+    if(len(ip) and len(username) and len(userpc)):
+        entry+="IP:"
+        entry+=ip
+        entry+=";UserName:"
+        entry+=username
+        entry+=";UserPC:"
+        entry+=userpc
+        print("entry=",entry)
     else:
-		print('it is unsupported command')
+        print("Error")
+        sys.exit(-1)
+    if options.register!=None:
+        print('it is for register action')
+        entry+=";DeviceID:"
+        entry+=options.register
+        cmd=['staf',options.ipaddr,'respool','add','pool','dji','entry',entry]
+		#p=subprocess.Popen(['staf',options.ipaddr,'respool','add','pool','dji','entry',options.register])
+        print("cmd=",cmd)
+        p=subprocess.Popen(cmd)
+        p.wait()
+        print('returncode=',p.returncode)
+        if p.returncode==0:
+            print("pass to reigster\n")
+        elif p.returncode==49:
+            print("already exists\n")
+        else:
+            print("fail to register\n")
+    elif options.unregister!=None:
+        print('it is for unregister action')
+        postcmd='CONFIRM'
+        entry+=";DeviceID:"
+        entry+=options.unregister
+        cmd=['staf',options.ipaddr,'respool','remove','pool','dji','entry',entry, 'confirm']
+        if options.force==True:
+            cmd.append('FORCE')
+        print("cmd=",cmd)
+        p=subprocess.Popen(cmd)
+        p.wait()
+        print('returncode=',p.returncode)
+        if p.returncode==0:
+            print("pass to unreigster\n")
+        elif p.returncode==48:
+            print("it does NOT exist\n")
+        elif p.returncode==4010:
+            print(" A resource pool entry you specified to REMOVE is owned.Use the -f or --force option if you are sure that the correct\
+                entry is specficed")
+        else:
+            print("fail to unregister\n")
+    else:
+        print('it is unsupported command')
+
+
 
 
