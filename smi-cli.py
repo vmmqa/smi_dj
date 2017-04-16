a47b2d4b77eafbe521c91109668b96aa8d657873
Libo Zhu <libo.zhu@intel.com>
2017-04-16 13:53:57 +0800

    improve smi client python by adding method to auto get ip/username/pcname
    Signed-off-by: Libo Zhu <libo.zhu@intel.com>

--- smi-cli.py
+++ smi-cli.py
@@ -1,35 +1,34 @@
 from optparse import OptionParser  
+import sys
+import os
+import socket
 import subprocess
 #from subprocess import Popen, PIPE
 #rc = subprocess.call(["ls","-l"])
 if __name__ == '__main__':
-	parser = OptionParser()
-	parser.add_option("-r", "--register", type="string", dest="register",  
+    usage = "usage: %prog [-r|-u boardid] [-f] [-i server's ip]"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-r", "--register", type="string", dest="register",  
                   help="format, ip:xxx;username:xxx;user:xxx")
-	parser.add_option("-i","--ip",type="string",dest="ipaddr",
+    parser.add_option("-i","--ip",type="string",dest="ipaddr",
                   help="please input the sever's ip",default="local")
-	parser.add_option("-u","--unregister",type="string",dest="unregister",
+    parser.add_option("-u","--unregister",type="string",dest="unregister",
                   help="please input the entry")
-	parser.add_option("-f", "--force",  
+    parser.add_option("-f", "--force",  
                   action="store_true", dest="force", default=False,  
                   help="force to unregister the HW on the server even if it is running")  
-	(options, args) = parser.parse_args()
-	print(options.register)
-	print(options.ipaddr)
-	print(options.unregister)
-	print(options.force)
-    #currently it only supports commands: list/launch
-    #out = subprocess.call("ls -l", shell=True)
-    #out = subprocess.call(["da"])
-    #out = subprocess.check_output(["ls","-l"])
-    #pattern='test'
-    #filename = 'tmp'
-    #p = subprocess.Popen(['grep', pattern, filename])
-    #output, error = p.communicate()
-    #p.wait()
-    #print('returncode=',p.returncode,'output=',output,'error=',error)
-
-	if options.register!=None:
+    (options, args) = parser.parse_args()
+    print(options.register)
+    print(options.ipaddr)
+    print(options.unregister)
+    print(options.force)
+    ip=socket.gethostbyname(socket.gethostname())
+    username=socket.gethostname()
+    userpc=os.getlogin()
+    entry=None
+    print("ip=",ip,",username=",username,",userpc=",userpc)
+    sys.exit()
+    if options.register!=None:
 		print('it is for register action')
 		cmd=['staf',options.ipaddr,'respool','add','pool','dji','entry',options.register]
 		#p=subprocess.Popen(['staf',options.ipaddr,'respool','add','pool','dji','entry',options.register])
@@ -43,7 +42,7 @@ if __name__ == '__main__':
 			print("already exists\n")
 		else:
 			print("fail to register\n")
-	elif options.unregister!=None:
+    elif options.unregister!=None:
 		print('it is for unregister action')
 		postcmd='CONFIRM'
 		cmd=['staf',options.ipaddr,'respool','remove','pool','dji','entry',options.unregister, 'confirm']
@@ -62,7 +61,7 @@ if __name__ == '__main__':
 			      entry is specficed")
 		else:
 			print("fail to unregister\n")
-	else:
+    else:
 		print('it is unsupported command')
 
 
