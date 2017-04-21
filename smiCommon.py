5f60c3d603694ff7023f1f7aa09b5da277f70d68
Libo Zhu <libo.zhu@intel.com>
2017-04-21 08:02:43 +0800

    add command example
    Signed-off-by: Libo Zhu <libo.zhu@intel.com>

--- smiCommon.py
+++ smiCommon.py
@@ -12,9 +12,17 @@ def cmdparse(entry):
         print("bad entry")
         ret=None
     return ret 
-    
+
+	    
 #if __name__ == '__main__':
 #    entry='ip:192.168.79.2;boardinfo:234;user:xiaoming'
 #    print('return value=',cmdparse(entry))
 
 
+#file copy 
+#staf $host_ip fs copy $opt "$host_file" todirectory "$target_path" tomachine $vm_ip
+#command executor
+#staf $vm_ip process start shell wait command "$target_cmd" returnstdout
+#may use shell-style
+#Goal: Execute the following shell-style command "grep 'Count = ' /tests/out | awk '{print $5}'" redirecting its standard output and standard error to /tests/awk.out.
+# add a workload support, which could stop all workload at the same time
