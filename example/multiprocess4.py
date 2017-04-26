e6feeb4419e804ecc2b8bee6460339c8063a7bf0
Libo Zhu <libo.zhu@intel.com>
2017-04-27 03:20:48 +0800

    update example to investigate process usage
    Signed-off-by: Libo Zhu <libo.zhu@intel.com>

--- example/multiprocess4.py
+++ example/multiprocess4.py
@@ -5,7 +5,6 @@ def f(l, i):
     l.acquire()
     try:
         print('hello world', i)
-        time.sleep(1)
     finally:
         l.release()
 
@@ -17,4 +16,6 @@ if __name__ == '__main__':
         p.start()
         print(p.is_alive())
         print(p.name)
+        p.join()
+        print(p.exitcode)
 
