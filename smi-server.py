4bebb97f14c118ee676403575ad608210ddf574d
Libo Zhu <libo.zhu@intel.com>
2017-04-28 15:11:47 +0800

    update server script
    add JobStatus/TestPlan/DeviceStatus update to SQL
    add timeout control
    integrate with windows script to define the reality script
    integrate with testplan to adhere to the specified rule
    
    Signed-off-by: Libo Zhu <libo.zhu@intel.com>

--- smi-server.py
+++ smi-server.py
@@ -8,6 +8,7 @@ import time
 import random
 from optparse import OptionParser  
 #==================
+#parse the entry into dict format
 def cmdparseMap(entry):
     lentry=entry.split(',')
     cmdLen=len(lentry)
@@ -19,6 +20,9 @@ def cmdparseMap(entry):
         dictEntry[key]=value
     return dictEntry
 
+#==================================
+#mySQL wrapper. it will execute subdir smi-sql.py
+#details usage refer to it
 def sqlAction(command):
     localCommand="python MysqlWrapper/smi-sql.py "
     localCommand+=command
@@ -29,12 +33,13 @@ def sqlAction(command):
         print("Fail to execute %s"%command)
     return ret
 
-# input worker
-def inputQ(queue,i):
-    info = str(os.getpid()) + '(put):' + str(time.time()) +'(number):'+str(i)
-    #print(info)
-    queue.put(info)
-def inputQ(queue,testplan,workspace,item, command):
+#===================================
+#use mutliprocess to lanuch multi jobs at the same time
+#the workspace is one for client
+#the item is the register entry
+#the command is the invoked wrapper script on client to lanuch job
+#the duration is the timeout for each job
+def inputQ(queue,testplan,workspace,item, command,duration):
     #currently two steps:
     #copy the testplan to remote machine specified dir
     #lanuch the common command on each ip
@@ -42,7 +47,11 @@ def inputQ(queue,testplan,workspace,item, command):
     entryDict=cmdparseMap(item)
     ip=entryDict["ClientIP"]
     deviceID=entryDict["DeviceID"]
-    print("ip=%s,deviceID=%s"%(ip,deviceID))
+    command+=deviceID
+
+    timestamp = strftime("%Y-%m-%d_%H-%M-%S", localtime())
+    command+=" "+timestamp
+    print("ip=%s,deviceID=%s,command=%s,duration=%s"%(ip,deviceID,command,duration))
     ##################################################
     #create and copy the testplan to remote machine specified dir
     ###################################################
@@ -71,12 +80,23 @@ def inputQ(queue,testplan,workspace,item, command):
     else:
 	    print('pass to execute cmdstep2')
 
+    ltestplan=testplan.split('/')
+    testplanName=ltestplan(len(ltestplan)-1)
+    preCommand=" "+item
+    preCommand+=" -u PlanName:"+testplanName
+    if(sqlAction(preCommand)==0):
+        print("pass to update PlanName value")
+    else:
+        print("fail to update PlanName value")
+        return
+
     ########################################################
     #lanuch the common command on each ip and put into queue
     #######################################################
-    cmdstep3='staf ' +ip+' process start shell command '
+    cmdstep3='staf ' +ip+' process start shell command "'
     cmdstep3+=command
-    cmdstep3+=' wait returnstdout'
+    cmdstep3+='" wait '+duration
+    cmdstep3+='  returnstdout stderrtostdout returnstderr'
     #process=os.popen(cmdstep3)
     print(cmdstep3)
     #tr=random.uniform(1,10) 
@@ -84,37 +104,31 @@ def inputQ(queue,testplan,workspace,item, command):
     #time.sleep(tr)
 
     preCommand=" "+item
-    preCommand+=" -u status:running"
+    preCommand+=" -u DeviceStatus:busy,JobStatus:running,JobStartTime:"
+
+    preCommand+=timestamp
     if(sqlAction(preCommand)==0):
         print("pass to update running status")
     else:
         return
     ret=subprocess.call(cmdstep3,shell=True)
-    #output="the result of ("
-    #output+=item
-    #output+=") and cmd=("
-    #output+=command
-    #output+=")"
-    #output+=str(ret)
-    #output+='\n'
         
     output=" "+item
-    output+=" -u status:"
+    output+=" -u DeviceStatus:free,JobStatus:"
     if ret==0:
         output+="pass"
     else:
         output+="fail"
-  #  output+=process.read()
- #   process.close()
-#    ret=process.exitcode
     print("item=%s,ret=%d\n"%(item,ret))
     queue.put(output)
     if(sqlAction(output)==0):
         print("pass to update pass/fail status")
     else:
         return
-    
+
+#===========================================
 # output worker
+#it reap all the lanuch queue
 def outputQ(queue,lock):
     info = queue.get()
     lock.acquire()
@@ -126,13 +140,14 @@ def outputQ(queue,lock):
 if __name__ == '__main__':
     optParser = OptionParser()
     optParser.add_option("-t","--testplan",action = "store",type="string",dest = "testplan")
-    optParser.add_option("-w","--workspace",action = "store",type="string",dest = "workspace",default="/home/sky-nuc-libo1/repos/dji/example/")
+    optParser.add_option("-d","--duration",action = "store",type="string",dest = "duration",default="1d",help="timeout for the longest test")
+    optParser.add_option("-w","--workspace",action = "store",type="string",dest = "workspace",default=r'D:\\ATF_Cloud_WorkSpace\\ATF_Client\\config')
     optParser.add_option("-r","--run",action = "append",dest = "run")
     #fakeArgs = ['-f','file.txt','-v','good luck to you', 'arg2', 'arge','-f','file2.txt']
-    #fakeArgs = ['-r','ip:192;broadinfo:xiaoming;user:xiaoming','-r','ip:193']  
     #options, args = optParser.parse_args(fakeArgs)
     options, args = optParser.parse_args()
     print(type(options.run),options.run)
+    print(type(options.duration),options.duration)
     print(type(options.testplan),options.testplan)
     record1 = []   # store input processes
     record2 = []   # store output processes
@@ -140,38 +155,33 @@ if __name__ == '__main__':
     queue = multiprocessing.Queue(3)
 
 
-    #ipl = [] #store all the board ip
-    #for item in options.run:
-    #    ipl.append(smiCommon.cmdparse(item))
-   # print(ipl)
     if(len(options.testplan)==0 or len(options.run)==0):
     	sys.exit()
     
-    #dic = {'192.168.79.1':'service list', 'local':'var list','192.168.79.2':'help help'}
-    #dic = {'10.239.196.96':'calc', 'local':'ls -l','10.239.159.58':'sleep 10s'}
-    cmd="sleep 2s"
+
+    cmd=r'pushed D:\\ATF_Cloud_WorkSpace\\ATF_Client\\ && python ATF_Main.py '
+
     # input processes
-    #for key in dic:
-        #process = multiprocessing.Process(target=inputQ,args=(queue,i))
     for item in options.run:
         process = multiprocessing.Process(target=inputQ,args=(queue,options.testplan,options.workspace,item,cmd))
         process.start()
         record1.append(process)
 
     # output processes
-    #for key in dic:
     for item in options.run:
         process = multiprocessing.Process(target=outputQ,args=(queue,lock))
         process.start()
         record2.append(process)
-
+    
+    #wait for the input Q to finish, default 1d
     for p in record1:
-        p.join()
+        p.join(86400)
 
     queue.close()  # No more object will come, close the queue
 
+    #wait for the output Q to finish
     for p in record2:
-        p.join()
+        p.join(86400)
 
     print("all process finishs!")
 
